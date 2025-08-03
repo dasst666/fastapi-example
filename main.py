ea04644a0aa3abef3cd3e18fc28c5a54ccd21ca6
Dastan <ddbolatkhan@gmail.com>
2025-08-03 22:50:25 +0500

    Пишу и тренирую фастапи тут

---
+++ main.py
@@ -0,0 +1,32 @@
+from typing import Annotated
+from fastapi import FastAPI, HTTPException, Query
+from pydantic import BaseModel
+
+app = FastAPI()
+
+@app.get("/hello/{name}")
+def hello_world(name, skip = 0):
+    return {"message": name, "skip": skip}
+
+class User(BaseModel):
+    name: str
+    number: int
+
+@app.post("/user")
+def create_user(user: User):
+    return user
+
+@app.get("/multiply/{a}/{b}")
+def multiply(a: int, b: int):
+    if a > 0 and b > 0:
+        return {"result": a * b}
+    else:
+        raise HTTPException(status_code=400, detail="a and b must be greater than 0")
+    
+@app.get("/users/filter")
+def filter_users(
+    min_age: Annotated[int, Query(gt=0)], 
+    max_age: Annotated [int | None, Query(gt=0, le=150)]):
+    if max_age is not None and max_age < min_age:
+        raise HTTPException(status_code=400, detail="max age is not lower than min age")
+    return {"filtered": True, "min_age": min_age, "max_age": max_age}
